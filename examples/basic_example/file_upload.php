5e4074b97c1172b5fd0558f59b8505d4a6813e95
Chad <webmastermeyers@gmail.com>
2017-07-18 09:50:45 -0300

    update example require path, and add upload folder

--- examples/basic_example/file_upload.php
+++ examples/basic_example/file_upload.php
@@ -1,9 +1,9 @@
 <?php
-require(dirname(__FILE__) . '/../../Uploader.php');
+require(dirname(__FILE__) . '/../../extras/Uploader.php');
 
 // Directory where we're storing uploaded images
 // Remember to set correct permissions or it won't work
-$upload_dir = '/upload_files/';
+$upload_dir = 'upload_files/';
 
 $uploader = new FileUpload('uploadfile');
 
@@ -11,7 +11,7 @@ $uploader = new FileUpload('uploadfile');
 $result = $uploader->handleUpload($upload_dir);
 
 if (!$result) {
-  exit(json_encode(array('success' => false, 'msg' => $uploader->getErrorMsg())));  
+  exit(json_encode(array('success' => false, 'msg' => $uploader->getErrorMsg())));
 }
 
 echo json_encode(array('success' => true));
